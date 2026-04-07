00818a06ea69a6dda2a6c3c8053737a207d6412c
HOMIES-HUB <qv62100@gmail.com>
2026-04-07 17:58:24 +0200

    TP1 ensemble

---
+++ TP1/script.js
@@ -0,0 +1,59 @@
+var list_items = [['Maison', 23999.99],['Manoir',2350000],['Cabane',0],['Appartement Marseille',199.99],
+['Maison Calais',10],['Hotêl de la plage',20]];
+
+
+let inputElt = document.getElementById("search-offer");
+inputElt.addEventListener("keyup", () => {
+    for (const location in locations) {
+        if (location.children)
+    }
+})
+
+let line = document.getElementsByClassName("line")[0];
+let products = document.getElementsByClassName("locations")[0];
+let i = 0
+let j = 0
+for (const item of list_items) {
+    if (i == 4) { // Pour ne pas dépasser 4 items par ligne
+        j += 1;
+        p = document.createElement("div");
+        p.classList.add("line");
+        products.appendChild(p);
+        line = document.getElementsByClassName("line")[j];
+        i = 0;
+    }
+    let div1 = document.createElement('div');
+    div1.classList.add("location");
+    let div_img = document.createElement('div');
+    div_img.classList.add("Image");
+    line.appendChild(div1);
+    div1.appendChild(div_img);
+    div2 = document.createElement('div');
+    div2.classList.add("info");
+    div1.appendChild(div2);   
+    para = document.createElement('p');
+    para.classList.add("name");
+    para.innerText = item[0];
+    div2.appendChild(para);
+    para = document.createElement('p');
+    para.classList.add("price");
+    para.innerText = item[1] + "€";
+    div2.appendChild(para);
+
+    detail = document.createElement('div');
+    detail.classList.add("detail");
+    //ajouter l'adresse tout ça dans 
+    div2.appendChild(para);
+
+    i += 1;
+    }
+
+var locations = document.getElementsByClassName("location");
+
+function showDetail(){
+    console.log("click");
+}
+
+for (i = 0; i < locations.length; i++) {
+    locations[i].addEventListener('click', showDetail, false);
+}

e88703fc9f0671aee5bfc132284d53b0f5d9b7fc
Anto Kajda <antoinekajda@gmail.com>
2026-05-12 16:07:24 +0200

    style

--- Projet/src/page/ComptePage.svelte
+++ Projet/src/page/ComptePage.svelte
@@ -15,8 +15,8 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <button class=" border-gray-300 shadow-sm p-1 rounded-sm bg-black text-white dark:bg-white dark:text-black" onclick={() => {localStorage.userId = ""; navigate("/login", {replace:true});}}>Déconnexion</button>
-    <button onclick={handleModeChange}>Changer mode</button>
+    <button class="border-gray-300 shadow-sm p-2 rounded-sm bg-black text-white dark:bg-white dark:text-black" onclick={() => {localStorage.userId = ""; navigate("/login", {replace:true});}}>Déconnexion</button>
+    <button class="pointer rounded-xl mt-4 shadow-xl shadow-black dark:shadow-white bg-black dark:bg-white text-white dark:text-black p-2 " onclick={handleModeChange}>Changer mode</button>
     <div class="w-full flex flex-col items-center">
         <span class="text-xl font-bold my-3 dark:text-gray-100">Mes logements</span>
         <div class="logements">

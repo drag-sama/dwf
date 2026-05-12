b1b2539afa48c1a9bd38c8db3e249e4f33fa0152
HOMIES-HUB <qv62100@gmail.com>
2026-05-12 15:29:11 +0200

    wip css details

--- Projet/src/page/DetailPage.svelte
+++ Projet/src/page/DetailPage.svelte
@@ -24,17 +24,21 @@
   <p>Chargement...</p>
 {:else if logement}
     <div class="flex flex-row dark:text-gray-200">
-        <div class="w-8/12">
+        <div class="w-7/12 mx-5">
             <Image src={logement.imageUrl}/>
-                <div>
-                    <p>{logement.prix}</p>
+                <div class="flex flex-col mt-5">
+                    <p>Prix: {logement.prix}€</p>
                     <p>Propriétaire: {proprietaire.prenom} {proprietaire.nom}</p>
                 </div>
         </div>
-        <div class="battlepass w-4/12">
-            <p>{logement.nom}</p>
-            <p>{logement.ville}</p>
-            <p>{logement.description}</p>
+        <div class="flex flex-col w-5/12">
+            <p class="font-bold self-center mt-10 text-xl mb-20">{logement.nom}</p>
+            <div class ="flex flex-col h-full text-lg">
+                <p class="mb-30">Ville: {logement.ville}</p>
+                <p>Description:</p>
+                <p>{logement.description}</p>
+            </div>
+            
     </div>
   </div>
 {/if}

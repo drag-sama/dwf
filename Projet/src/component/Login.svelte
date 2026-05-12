81203cac9e79eeac21a4202af4592b7f8f1001b0
HOMIES-HUB <qv62100@gmail.com>
2026-05-12 09:20:02 +0200

    text email incorrect

--- Projet/src/component/Login.svelte
+++ Projet/src/component/Login.svelte
@@ -7,12 +7,17 @@ import {userName} from "../lib/store"
         const res = await fetch("/api/utilisateurs")
         users = await res.json()
     }
+    let failedlogin = $state(false)
     getUsers()
     const login = () => {
         console.log(email + ' oui')
         let actualUser = users.filter((user) => user.email == email)
         if(actualUser.length > 0){
             userName.set(actualUser[0].id)
+            failedlogin = false;
+        }
+        else {
+            failedlogin = true;
         }
     }
 </script>
@@ -23,7 +28,8 @@ import {userName} from "../lib/store"
         login()}}>
 
 <span class="mt-3 text-3xl">Connexion</span>
-    <img src="/Logo.png" alt="Logo" class="size-40 mb-10 mx-15"/>
+    <img src="/Logo.png" alt="Logo" class="size-40 mb-5 mx-15"/>
+    <p class={failedlogin ? "text-red-500 mb-3" : "text-white mb-3"}>Email incorrect</p>
     <input class="border rounded-sm p-1 border-gray-300 mb-5" bind:value={email} placeholder="Adresse email" />
     <button class="shadow-sm mb-5 p-2 rounded-xl bg-black text-white">Connecter</button>
 </form>

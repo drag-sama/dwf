90950e92372d316330bd149ce580f96f933a5ea4
HOMIES-HUB <qv62100@gmail.com>
2026-05-12 14:54:26 +0200

    css detail wip

--- Projet/src/page/DetailPage.svelte
+++ Projet/src/page/DetailPage.svelte
@@ -3,20 +3,38 @@
 
     let {id} = $props()
     import { onMount } from 'svelte';
+    import Image from '../component/Image.svelte';
     let logement = $state(null);
+    let proprietaire = $state(null);
     let loading = $state(true);
 
     onMount(async () => {
         const res = await fetch(`/api/logements/${id}`);
         logement = await res.json();
+        const res2 = await fetch(`/api/utilisateurs/${logement.proprietaireId}`)
+        proprietaire = await res2.json();
         loading = false;
     });
+
+
+
 </script>
 
 {#if loading}
   <p>Chargement...</p>
 {:else if logement}
-  <h1>{logement.nom}</h1>
-  <p>{logement.ville}</p>
-  <p>{logement.prix} €</p>
+    <div class="flex flex-row dark:text-gray-200">
+        <div class="w-8/12">
+            <Image src={logement.imageUrl}/>
+                <div>
+                    <p>{logement.prix}</p>
+                    <p>Propriétaire: {proprietaire.prenom} {proprietaire.nom}</p>
+                </div>
+        </div>
+        <div class="battlepass w-4/12">
+            <p>{logement.nom}</p>
+            <p>{logement.ville}</p>
+            <p>{logement.description}</p>
+    </div>
+  </div>
 {/if}
